20d0c85e8b602cf9e51247cfec4ddcbd0bbde0f6
MonsieurWave <tensu.wave@gmail.com>
2016-08-29 12:29:28 -0400

    Set to fullscreen Deleted CalibrationActivity

--- app/src/main/java/monsieurwave/earguard/MainActivity.java
+++ app/src/main/java/monsieurwave/earguard/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ToggleButton;
@@ -24,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+//Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
         setContentView(R.layout.activity_main);
         Log.w("MainActivity", "onCreate called");
         addListenerOnButton();
